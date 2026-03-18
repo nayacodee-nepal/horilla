2ad1548b62b80c21ff1a7f8eb69d89e03286e6b4
nayacodee-nepal <nayacodee@gmail.com>
2026-03-18 12:09:38 +0545

    Update settings.py

--- horilla/settings.py
+++ horilla/settings.py
@@ -174,7 +174,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
